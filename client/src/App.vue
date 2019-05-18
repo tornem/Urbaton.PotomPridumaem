2ee61ea55517be796d82c116b6c09fe2d1b8098b
Danil Chugaev <d.chugaev@n1.ru>
2019-05-19 04:01:59 +0700

    update app

--- client/src/App.vue
+++ client/src/App.vue
@@ -63,12 +63,6 @@ export default {
       isOpenMenu: false,
     };
   },
-  watch: {
-    '$route' (to) {
-      // необходимо для перезагрузки
-      this.$router.push(to.path);
-    }
-  },
   computed: {
     isHomePage() {
       return this.$route.path === '/';
