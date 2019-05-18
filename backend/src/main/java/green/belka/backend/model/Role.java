e88aff9316f591ae22bf705d1ff81cbbce2f699f
cherdakov <eqx11@yandex.ru>
2019-05-18 23:47:51 +0700

    fix for misha

--- backend/src/main/java/green/belka/backend/model/Role.java
+++ backend/src/main/java/green/belka/backend/model/Role.java
@@ -1,24 +1,11 @@
 package green.belka.backend.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "role")
-public class Role {
-
-    @Setter
-    @Getter
-
-    @Id
-    @Column(name = "role_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @OneToOne(optional=false, mappedBy="role")
-    private Long RoleId;
-
-    @Column(name = "role")
-    private Long role;
-
+public enum Role {
+    ADMIN,
+    USER
 }
